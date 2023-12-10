a0fdb506658bbb95bae54db846c764cd81361c20
Stephen Martin <stephenmartin2951@pm.me>
2023-12-10 16:43:27 -0600

    Initial Commit

---
+++ DessertList/DessertListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertListApp.swift
+//  DessertList
+//
+//  Created by Stephen Martin on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
